9b1f40fb6f0739883fc924aae7213ec9f2b512c0
blen_meku <mekuriablen@gmail.com>
2020-12-03 20:42:49 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+class Polygon {
+    constructor(side) {
+        this.side = side;
+    }
+    get countSides(){ return this.side.length}
+    
+    get perimeter(){ 
+        let p = 0
+        this.side.forEach(s => { p +=s
+        });
+        return p;
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid() {
+        if (this.countSides !== 3) { return false }
+        const s1 = this.side[0]
+        const s2 = this.side[1]
+        const s3 = this.side[2]
+        if (s1 < (s2 + s3) && s2 < (s1 + s3) && s3 < (s1 + s2)) { return true }
+        else { return false }
+    }
+}
+class Square extends Polygon {
+    get isValid() {
+        if (this.countSides !== 4) return false
+        const s1 = this.side[0]
+        const s2 = this.side[1]
+        const s3 = this.side[2]
+        const s4 = this.side[3]
+        return (s1 === s2 && s2 === s3 && s3 === s4)
+    }
+    get area(){
+        let a = 0;
+        this.side.forEach(s => {
+           a = s * s;
+        });
+        return a;
+    }
+}
