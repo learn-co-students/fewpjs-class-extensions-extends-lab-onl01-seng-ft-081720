6113b217b8a64b235673f07d7ff899c927bbbab5
kurwitz3 <kurwitz3@aol.com>
2020-12-08 20:53:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+class Polygon{
+    constructor(array){
+        this.array = array 
+
+    }
+    get perimeter(){
+     return  this.array.reduce((acc,value) => acc + value,0)
+    }
+    get countsides(){
+       this.array.map(e=> e.length)
+    }
+    
+}
+
+class Triangle extends Polygon{
+
+}
+
+class Square extends Polygon{
+   get isvalid(){
+      this.array.every((val,i,arr)=> val === arr[0])
+   }
+}
